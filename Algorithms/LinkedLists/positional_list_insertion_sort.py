635e95dbe0619abc632070455e7854d4c6ade346
ashley speight <ashleyspeight86@gmail.com>
2023-01-25 22:31:30 +0100

    positional lists

---
+++ Algorithms/LinkedLists/positional_list_insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(L):
+    """Sort positionallist of comparable elements into nondecreasing order"""
+    if len(L) > 1:
+        marker = L.first()
+        while marker != L.last():
+            pivot = L.after(marker)
+            value = pivot.element()
+            if value > marker.element():
+                marker = pivot
+            else:
+                walk = marker
+                while walk != L.first() and L.before(walk).element() > value:
+                    walk = L.before(walk)
+                L.delete(pivot)
+                L.add_before(walk, value)
